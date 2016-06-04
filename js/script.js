d6718992833d323b6736c2a4a73019f97cb46693
Joseph Anda <josephanda@Josephs-MacBook-Pro.local>
2016-06-04 13:11:59 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+//[  ]  Build this with MVC
+//[  ]  Make a locking menu that changes transparency as you scroll
+//[  ]  Stop mixing font size specification types (like px and em)
+//[  ]  Format footer text properly  
