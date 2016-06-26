c4ece6602a8653bdb15e90e2e6935ee94201c7d9
Joseph Anda <josephanda@Josephs-MacBook-Pro.local>
2016-06-25 17:01:38 -0700

    added first mockup changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,38 @@
 //[  ]  Build this with MVC
-//[  ]  Make a locking menu that changes transparency as you scroll
 //[  ]  Stop mixing font size specification types (like px and em)
-//[  ]  Format footer text properly  
+//[!!]  Format footer text properly  
+//[  ]  Occupy top of page with logo
+//[!!]  Make the signup button 'louder'
+//[  ]  Incorporate transitioning images (ask Manny and Daniel for ideas)
+
+(function() {
+
+	"use strict";
+
+	var model = {
+
+		init: function() {
+			console.log("model initialized") //<--For debugging
+		}
+
+	};
+
+	var view = {
+
+		init: function() {
+			console.log("view initialized")  //<--also for debugging
+		}
+
+	};
+
+	var controller = {
+
+		init: function() {
+			model.init();
+			view.init();
+		}
+	};
+
+	controller.init();
+}) ();
+	
