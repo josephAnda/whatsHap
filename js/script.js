5a57e62142a0bff2951ab7d771744df5d785a01f
Joseph Anda <josephanda@Josephs-MacBook-Pro.local>
2016-07-11 13:47:45 -0700

    added locking bar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //[!!]  Build this with MVC
 //[  ]  Stop mixing font size specification types (like px and em)
 //[!!]  Format footer text properly  
-//[  ]  Occupy top of page with logo
+//[!!]  Occupy top of page with logo
 //[!!]  Make the signup button 'louder'
 //[  ]  Incorporate transitioning images (ask Manny and Daniel for ideas)
 
@@ -30,8 +30,8 @@
 			$(window).scroll(function() {    
 			    var scroll = $(window).scrollTop();
 			    console.log(scroll);  // <--- Useful for debugging the scroll-dependent menu-fixing 
-			    if (scroll >= 100) {
-			    	console.log("This should work", model.navForm);
+			    if (scroll > 0) {
+			    	//console.log("This should work", model.navForm);
 			        model.navForm.addClass("taskbar-scroll");
 			        model.phantom.css({position: "relative"});
 			    } else {
