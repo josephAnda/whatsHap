11579401c0b962ebf12d2786e7e38d0d8494ae45
Joseph Anda <josephanda@Josephs-MBP.wp.comcast.net>
2016-08-02 18:10:21 -0700

    created responsive layout

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 //[!!]  Occupy top of page with logo
 //[!!]  Make the signup button 'louder'
 //[  ]  Incorporate transitioning images (ask Manny and Daniel for ideas)
+//[  ]  Add function to add an inline display class when viewport width shrinks 
 
 (function() {
 
@@ -15,8 +16,8 @@
 			console.log("model initialized") //<--For debugging
 		},
 		navForm: $('#navForm'),
-		phantom: $('#phantom')  // <---This is to keep the non-taskbar content from re-flowing once the taskbar locks via position: fixed
-
+		phantom: $('#phantom'),  // <---This is to keep the non-taskbar content from re-flowing once the taskbar locks via position: fixed
+		signUpForm: $('#responsiveForm')
 
 	};
 
@@ -25,6 +26,7 @@
 		init: function() {
 			console.log("view initialized");  //<--also for debugging
 			view.scrollLock();
+			view.makeInlineForm();
 		},
 		scrollLock: function() {
 			$(window).scroll(function() {    
@@ -39,7 +41,30 @@
 			        //model.phantom.css({position: "fixed", visibility: "hidden"});  
 			    }
 			})
-		}
+		}//,  (The function below was made obsolete . . . . I am choosing not to use this method of making the form responsive)
+		//  However, it might still be useful in the future so for now it is just commented out.  
+		//makeInlineForm: function() {
+			//var max_width = 887;
+			//  The function below correctly changes the classes, but fails to create the desired effect
+			//  [  ]  Fix the problem described above (try using a form-group div, experiment with bootstraps version)
+		    
+		    //$(window).resize(function() {
+		    	// cross-browser stack overflow width dimension 
+		    	//var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+		    	//if (w < 887) {
+		    		//model.signUpForm.removeClass("navbar-form");
+		    		//model.signUpForm.removeClass("navbar-right");
+		    		//model.signUpForm.addClass("form-inline");
+		    	//} else {
+		    		//model.signUpForm.removeClass("form-inline");
+		    		//model.signUpForm.addClass("navbar-form");
+		    		//model.signUpForm.addClass("navbar-right");
+		    	//}
+		    	
+  			    //console.log('the width is ' + w); // <--More debugging tools below
+  			    //console.log(model.signUpForm);
+			//});
+		//}
 
 	};
 
