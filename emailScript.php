f2094bf5887cd5f81fd6209024e28a2785ce53b5
Joseph Anda <josephanda@Josephs-MacBook-Pro.local>
2016-11-16 21:23:45 -0800

    changed email

--- emailScript.php
+++ emailScript.php
@@ -15,8 +15,8 @@
 	echo 'Debug line 13 (require completed) <br />';
 	// Create the Transport.  Using google's SMTP requires a gmail account that allows less secure apps to access the sign in credentials.  This is not the default setting and must be updated in 'Sign in and Security' in the 'My Account' section of the settings portion 
 	$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
-	  ->setUsername('orenmurasaki@gmail.com')
-	  ->setPassword('****')  //  <-- [  ]  Hash this
+	  ->setUsername('whatshapinfo@gmail.com')
+	  ->setPassword('JDMourdomain123')  //  <-- [  ]  Hash this
 	;
 
 	echo 'Debug line 20 (transport set) <br />';
@@ -27,9 +27,9 @@
 	$message = Swift_Message::newInstance()
 
 		// Give the message a subject
-		->setSubject('New User Signup Test')
+		->setSubject('New User Signup')
 		// Set the From address with an associative array
-		->setFrom(array('orenmurasaki@gmail.com' => 'Joseph Anda'))
+		->setFrom(array('whatshapinfo@gmail.com' => 'WhatsHap Info'))
 		// Set the To addresses with an associative array
 		->setTo(array('orenmurasaki@gmail.com' => 'Joe'))
 		// TESTING FEATURE	Set CC . . . to you guys' emails
